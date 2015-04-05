af77299791723cc1c19e8a221508e7484123cd90
Richard Terrell <rickalo801@gmail.com>
2015-04-04 20:30:45 -0700

    Setup pure websockets, without socket.io. Added sugar.js. Super messy still.

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var WebSocketServer = require('ws').Server;
+var http = require('http');
+var express = require('express');
+var app = express();
+var sugar = require('sugar');
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+//var http = require('http').Server(app);
+//var io = require('socket.io')(http);
+
+//app.get('/', function(req, res){
+//    res.sendFile(__dirname + '/index.html');
+//});
+app.use(express.static(__dirname + '/'));
+
+var server = http.createServer(app);
+server.listen(8080);
+
+var clients = [];
+var wss = new WebSocketServer({server: server});
+wss.on('connection', function(ws) {
+    clients.push(ws);
+    //clients << ws;
+    console.log("client count: " + clients.count());
+
+    console.log("user connected");
+    ws.on('message', function(message) {
+        console.log('received: %s', message);
+        clients.forEach(function(client) {
+            if(client != ws) client.send(message);
+        });
+    });
+    ws.onclose = function(event) {
+        console.log("Closing a socket.");
+        clients.remove(ws);
+        //clients.filter(function(client){return client !== ws;})
+    };
+//    ws.send('something');
 });
 
+
+/*
 io.on('connection', function(socket){
     console.log('a user connected');
     socket.on('disconnect', function(){
@@ -16,7 +49,8 @@ io.on('connection', function(socket){
         socket.broadcast.emit('chat message', msg);
     });
 });
+*/
 
-http.listen(3000, function(){
-    console.log('listening on *:3000');
-});
+//http.listen(3000, function(){
+//    console.log('listening on *:3000');
+//});
